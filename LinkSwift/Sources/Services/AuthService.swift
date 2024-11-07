6352f67278a6b975c963c0fd6e5b213c742733e4
yanislammari <yanis.lammari937@gmail.com>
2024-11-08 00:23:23 +0100

    add authentification register service

--- LinkSwift/Sources/Services/AuthService.swift
+++ LinkSwift/Sources/Services/AuthService.swift
@@ -40,4 +40,56 @@ class AuthService {
         }
         task.resume()
     }
+    
+    static func register(firstname: String, lastname: String, email: String, password: String, profilePicture: Data, completion: @escaping () -> Void) {
+        var body: Data = Data()
+        let url: URL = URL(string: "\(Api.baseUrl)/auth/register")!
+        var request: URLRequest = URLRequest(url: url)
+        let boundary: String = UUID().uuidString
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"firstName\"\r\n\r\n".data(using: .utf8)!)
+        body.append("\(firstname)\r\n".data(using: .utf8)!)
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"lastName\"\r\n\r\n".data(using: .utf8)!)
+        body.append("\(lastname)\r\n".data(using: .utf8)!)
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"email\"\r\n\r\n".data(using: .utf8)!)
+        body.append("\(email)\r\n".data(using: .utf8)!)
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"password\"\r\n\r\n".data(using: .utf8)!)
+        body.append("\(password)\r\n".data(using: .utf8)!)
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"profilePicture\"; filename=\"profile.jpg\"\r\n".data(using: .utf8)!)
+        body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+        body.append(profilePicture)
+        body.append("\r\n".data(using: .utf8)!)
+        body.append("--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        request.httpBody = body
+        
+        let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] else {
+                return
+            }
+            DispatchQueue.main.async {
+                if let message = json["message"] as? String {
+                    self.message = message
+                }
+                if let token = json["token"] as? String {
+                    self.token = token
+                }
+                completion()
+            }
+        }
+        task.resume()
+    }
 }
